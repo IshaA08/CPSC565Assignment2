6cf3173e4ceb7aa1d9a83e9db94aa2edb57198a2
Isha Afzaal <isha_afzaal8@outlook.com>
2021-02-26 21:25:31 -0700

    Player attraction to snitch implemented

--- Scripts/Agent1.cs
+++ Scripts/Agent1.cs
@@ -1,18 +1,30 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Agent1 : MonoBehaviour
 {
+    private GameObject snitch;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        snitch = GameObject.Find("Snitch");
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        
+ 
+    }
+
+
+
+    public float attractForce = 3.0f;
+    void FixedUpdate()
+    {
+        Vector3 direction = snitch.transform.position - transform.position;
+        gameObject.GetComponent<Rigidbody>().AddForce(attractForce * direction);
+
     }
 }
